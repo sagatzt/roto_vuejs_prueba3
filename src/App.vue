c93ce04640bd5d0587ff8135720481cece670a46
sagatzt <sagatzt@gmail.com>
2021-03-14 10:05:29 +0100

    nuevo commit h1

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,7 @@
     </div>
   </div>
   <router-view />
+  <h1>Este es un nuevo commit</h1>
   <div class="nota">
     <p><b>NOTA: </b>Debéis crear una función para que el password aparezca
     con asteriscos. Junto al password habrá un icono de un ojo abierto que,
